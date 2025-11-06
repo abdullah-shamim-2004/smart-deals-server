e59828a1aed163595d2f613f210f60faf1de41fb
abdullah-shamim-2004 <abdullahshamim884@gmail.com>
2025-11-06 16:15:15 +0600

    Add middleware for verify firebase token.

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const admin = require("firebase-admin");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+
+const serviceAccount = require("./smart-deals-firebase-adminsdk.json.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 // 🔹 Middleware
 app.use(cors());
 app.use(express.json());
+// Firebase tokan verify
+const VerifyFireBaseTokan = async (req, res, next) => {
+  const headerAuth = req.headers.authorization;
+
+  if (!headerAuth) {
+    //do not allow to go
+    return res.status(401).send({ message: "Unothorized access" });
+  }
+  const token = headerAuth.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Unothorized access" });
+  }
+  try {
+    const userInfo = await admin.auth().verifyIdToken(token);
+    req.token_email = userInfo.email;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Unothorized access" });
+  }
+};
 
 const uri = process.env.MONGODB_URL;
 
@@ -27,6 +55,22 @@ async function run() {
     //create database and collection
     const db = client.db("smart_db");
     const productsCollection = db.collection("products");
+    const bidsCollection = db.collection("bids");
+    const userCollection = db.collection("users");
+
+    // Post/create user information
+    app.post("/user", async (req, res) => {
+      const { email } = req.body;
+      // Check if user already exists
+      const existingUser = await userCollection.findOne({ email });
+      if (!existingUser) {
+        const newUser = req.body;
+        await userCollection.insertOne(newUser);
+        return res.status(201).json({ message: "New user added" });
+      } else {
+        return res.status(200).json({ message: "User already exists" });
+      }
+    });
 
     // Create a products Collection (POST)
     app.post("/products", async (req, res) => {
@@ -38,24 +82,69 @@ async function run() {
       }
     });
 
-    //Get alldata
-    app.get("/products", async (req, res) => {
+    //Get bids (GET)
+    app.get("/bids", VerifyFireBaseTokan, async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+
+      if (email) {
+        if (email !== req.token_email) {
+          return res.status(403).send({ message: "Forbidden access" });
+        }
+        query.buyerEmail = email;
+      }
+
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Create a bids Collection (POST)
+    app.post("/bids", async (req, res) => {
+      try {
+        const bids = await bidsCollection.insertOne(req.body);
+        res.status(201).json(bids);
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+    });
+
+    //Get all Products
+    app.get("/allproducts", async (req, res) => {
       const cursor = productsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    //Get a single data
-    app.get("/products/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await productsCollection.findOne(query);
+    // Get latest products
+    app.get("/latest-products", async (req, res) => {
+      const cursor = productsCollection
+        .find()
+        .sort({ created_at: -1 })
+        .limit(6);
+      const result = await cursor.toArray();
       res.send(result);
     });
 
+    //Get a single data
+    app.get("/allproducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await productsCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+    //Get bids for a single product
+    app.get("/allproducts/bids/:productid", async (req, res) => {
+      const id = req.params.productid;
+      const query = { ProductId: id };
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     //Update Product
-    app.patch("/products/:id", async (req, res) => {
+    app.patch("/allproducts/:id", async (req, res) => {
       const id = req.params.id;
       const updateProduct = req.body;
       const query = { _id: new ObjectId(id) };
@@ -67,7 +156,7 @@ async function run() {
     });
 
     //Delete a product
-    app.delete("/products/:id", async (req, res) => {
+    app.delete("/allproducts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await productsCollection.deleteOne(query);
